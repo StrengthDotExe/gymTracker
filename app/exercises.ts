1602eb38d5b00d8d56aa33251b598c6fa921d102
megu02 <47181238+megu02@users.noreply.github.com>
2025-11-13 22:12:52 +0100

    Create exercises.ts

---
+++ app/exercises.ts
@@ -0,0 +1,13 @@
+export type Exercise = {
+  name: string;
+  sets: number;
+  reps: number;
+};
+
+export const exerciseList: Exercise[] = [
+  { name: 'Brzuszki', sets: 3, reps: 15 },
+  { name: 'Przysiady', sets: 4, reps: 12 },
+  { name: 'Pompki', sets: 3, reps: 10 },
+  { name: 'Plank', sets: 3, reps: 60 }, 
+  { name: 'Skakanka', sets: 5, reps: 100 },
+];
