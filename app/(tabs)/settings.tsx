1f46b890a2cbbae19cf631849dda9698d471d578
Chester133 <monstersolider0@gmail.com>
2025-11-13 23:09:03 +0100

    setting 1.2

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -42,7 +42,7 @@ export default function ModalScreen() {
     {
       f: toggleSwitch2,
       b: isEnabled2,
-      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
+      id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28bb',
       title: 'Powiadominia',
     },
   ];
